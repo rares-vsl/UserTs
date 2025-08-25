8d0694125e1b256e83cb6809b4da68b949d80544
Vasiliu Rares <rv.vasiliu@gmail.com>
2025-08-25 11:47:56 +0200

    build: first build
    Signed-off-by: Vasiliu Rares <rv.vasiliu@gmail.com>

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+import io.github.andreabrighi.gradle.gitsemver.conventionalcommit.ConventionalCommit
+
+plugins {
+    alias(libs.plugins.gitSemVer)
+}
+
+buildscript {
+    dependencies {
+        classpath("io.github.andreabrighi:conventional-commit-strategy-for-git-sensitive-semantic-versioning-gradle-plugin:1.0.0")
+    }
+}
+
+gitSemVer {
+    commitNameBasedUpdateStrategy(ConventionalCommit::semanticVersionUpdate)
+    assignGitSemanticVersion()
+}
